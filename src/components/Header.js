838b2954c99b65ca8e8924585038ffb7c11d568a
divyanshukaushik <divyanshukaushik33@gmail.com>
2021-10-30 20:03:27 +0530

    setup react router

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavDropdown, Nav, Container, Navbar } from "react-bootstrap";
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="bg-secondary" expand="lg">
+        <Container>
+         <Link to="/" className="links" exact> <Navbar.Brand> Social CRM</Navbar.Brand></Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Index() {
+    return (
+        <div className="container col-lg-6 mx-auto">
+          <h1 className="text-center fs-1">Social CRM</h1>
+        </div>
+    )
+}
+
+export default Index
