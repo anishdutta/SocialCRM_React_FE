e0ed089160c2f94103369960422257d91eb5aed8
Sahaj Ghatiya <sahajghatiya531.sg@gmail.com>
2021-11-17 09:01:28 +0530

    linkedin oauth

--- src/components/Login/Linkedin.js
+++ src/components/Login/Linkedin.js
@@ -1,4 +1,5 @@
 
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { LinkedIn } from "react-linkedin-login-oauth2";
 // import { useLinkedIn } from "react-linkedin-login-oauth2";
@@ -26,31 +27,78 @@ const App = () => {
     //     handleFailure(error);
     //   },
     // }); 
+    const redirecturi = "http://localhost:3000/linkedin"
+    const urlencoded = "http%3A%2F%2Flocalhost%3A3000%2Flinkedin"
+    const clientid = "782lxko1czd0k0"
+    const clientsecret = "5yv3xigfyzedLIT4"
+
+    // const handlelinkedin = () => {
+    //     axios.post(`https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientid}&redirect_uri=${redirecturi}&state=foobarstate&scope=r_liteprofile%20r_emailaddress%20w_member_social`).then(data => {
+    //         console.log(data)
+    //     })
+    // }
+
+
+    useEffect(() => {
+        let url = window.location.href;
+        let params = (new URL(url)).searchParams;
+        console.log(params.get('code'))
+        const code = params.get('code')
+        setcode(code)
+
+        if (code) {
+            try {
+                getAccessToken(code)
+                // axios.post('https://www.linkedin.com/oauth/v2/accessToken', {
+                //     'grant_type': 'authorization_code',
+                //     'code': code,
+                //     'redirect_uri': urlencoded,
+                //     'client_id': clientid,
+                //     'client_secret': clientsecret
+                // }).then(response => {
+                //     console.log(response)
+                // }).catch(data => { console.log(data) })
+            } catch {
+                console.log("error")
+
+            }
+        }
+    }, [])
+
+
+
+    const link = ` https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientid}&redirect_uri=${redirecturi}&state=foobarstate&scope=r_liteprofile%20r_emailaddress%20w_member_social`
+    // const getcode = () => {
+    //     const link = ` https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientid}&redirect_uri=${redirecturi}&state=foobarstate&scope=r_liteprofile%20r_emailaddress%20w_member_social`
+    //     return link
+    // }
+
+    const getAccessToken = (code) => {
+        const url = "https://www.linkedin.com/oauth/v2/accessToken"
+        const params = {
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': urlencoded,
+            'client_id': clientid,
+            'client_secret': clientsecret
+        }
+        axios.post(url, params).then(data => { console.log(data) }).catch(err=>console.log(err))
+    }
+
+
     return (
         <div>
-            <LinkedIn
-                clientId="782lxko1czd0k0"
-                redirectUri={`${window.location.origin}/linkedin`}
-                onSuccess={(code) => {
-                    handleSuccess(code)
-                    localStorage.setItem("linkedin", JSON.stringify(code));
-                }}
-                onError={(error) => {
-                    handleFailure(error)
-                    console.log(error);
-                }}
-            >
-                {({ linkedInLogin }) => (
-                    <img
-                        onClick={linkedInLogin}
-                        src={
-                            "https://cdn.pixabay.com/photo/2017/08/22/11/56/linked-in-2668700_960_720.png"
-                        }
-                        alt="Sign in with Linked In"
-                        style={{ maxWidth: "180px", cursor: "pointer" }}
-                    />
-                )}
-            </LinkedIn>
+
+            <a href={link}>
+                <img
+                    src={
+                        "https://cdn.pixabay.com/photo/2017/08/22/11/56/linked-in-2668700_960_720.png"
+                    }
+                    alt="Sign in with Linked In"
+                    style={{ maxWidth: "180px", cursor: "pointer" }}
+                />
+            </a>
+
             {!code && <div>No code</div>}
             {code && <div>Code: {code}</div>}
             {errorMessage && <div>{errorMessage}</div>}
---
+++ src/pages/Linkedin.js
@@ -0,0 +1,43 @@
+
+import React from "react";
+// import LatestPost from "../components/Posts/LatestPost";
+// import LatestComment from "../components/Posts/LatestComment";
+import "../components/Posts/posts.css";
+// import LatestMessage from "../components/Posts/LatestMessage";
+
+// import axios from "axios";
+
+import { useEffect, useState } from "react";
+import LiLogin from "../components/Login/Linkedin";
+// import { Link, useHistory, useParams } from "react-router-dom";
+
+function Post() {
+    // const accessid = localStorage.getItem("liaccesstoken");
+    // const redirecturi = "http://localhost:3000/linkedin"
+    // const urlencoded = "http%3A%2F%2Flocalhost%3A3000%2Flinkedin"
+    // const clientid = "782lxko1czd0k0"
+    // const clientsecret = "5yv3xigfyzedLIT4"
+
+
+
+    // const page_id = localStorage.getItem("lipageid");
+    // // const page_id = useRecoilValue(pageid);
+    // //   const [item, setItem] = useState("");
+    // const [comments, setcomments] = useState([]);
+    // const [isloading, setisloading] = useState(true);
+    // const [textInput, setTextInput] = React.useState("");
+
+    // const [isLoggedin, setisLoggedin] = useState(false);
+
+    // const [selectpostcomments, setSelectpostcomments] = useState(null);
+    // const [selectpostid, setSelectpostid] = useState(null);
+
+
+    return (
+        <div className="h-100 d-flex align-items-center justify-content-center">
+            <LiLogin />
+        </div>
+    )
+}
+
+export default Post;
